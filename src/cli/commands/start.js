dfae2fe12a81e4f7b34254381ec630873d46850c
Artem Korolev <artkor@live.ru>
2018-10-21 19:54:38 +0300

    (fix): switching from deprecated webpack-serve to webpack-dev-server

--- src/cli/commands/start.js
+++ src/cli/commands/start.js
@@ -1,15 +1,38 @@
-function start() {
+const defaultStatsOptions = {
+	colors: true,
+};
+const defaultDevServerOptions = {
+	stats: defaultStatsOptions,
+};
+const defaultServerOptions = {
+	port: 3000,
+	host: "localhost",
+};
+
+function start(userDevServerOptions) {
 	process.env.NODE_ENV = "development";
 
-	const serve = require("webpack-serve"); // eslint-disable-line global-require
+	const options = {
+		...defaultDevServerOptions,
+		...userDevServerOptions,
+	};
 
-	const argv = {};
 	const webpackConfig = require("@omgjs/webpack-config-react"); // eslint-disable-line global-require
 
-	serve(argv, { config: webpackConfig }).then(server => {
-		server.on("listening", ({ options }) => {
-			console.log(`Webpack serving application on port ${options.port}`); // eslint-disable-line no-console
-		});
+	const webpackLib = require("webpack"); // eslint-disable-line global-require
+	const WebpackDevServer = require("webpack-dev-server"); // eslint-disable-line global-require
+
+	const { port, host } = { ...defaultServerOptions };
+	const server = new WebpackDevServer(webpackLib(webpackConfig), options);
+
+	server.listen(port, host, error => {
+		if (error) {
+			console.log(error); // eslint-disable-line no-console
+		}
+
+		console.log(error); // eslint-disable-line no-console
+
+		console.log(`Webpack serving app on http://${host}:${port}`); // eslint-disable-line no-console
 	});
 }
 
