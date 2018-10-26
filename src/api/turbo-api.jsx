0a601e30bcd8da1f11f77899b0ce1ace526b40f7
Artem Korolev <artkor@live.ru>
2018-10-26 16:52:24 +0300

    feat(function): helper function added - componentWithPropTypes Example usage: import React from "react"; import { componentWithPropTypes } from "@omgJS/turbo"; import { shape, func } from "prop-types";
    export default componentWithPropTypes(
    	({ history }) => (
    		<div>
    			Page with props
    			<button type="button" onClick={() => history.goBack()}>
    				Go back
    			</button>
    		</div>
    	),
    	{
    		history: shape({
    			goBack: func.isRequired,
    		}).isRequired,
    	},
    );

--- src/api/turbo-api.jsx
+++ src/api/turbo-api.jsx
@@ -4,19 +4,69 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { createStore } from "redux";
 import { connect, Provider } from "react-redux";
 
-function addRoute(path, routes) {
+function connectToRedux(component) {
 	const mapStateToProps = state => ({
 		todos: state.todos,
 	});
 	const mapDispatchToProps = dispatch => ({
 		dispatch,
 	});
-
-	const connectedComponent = connect(
+	return connect(
 		mapStateToProps,
 		mapDispatchToProps,
-	)(routes[path]);
-	return <Route exact key={path} path={path} component={connectedComponent} />;
+	)(component);
+}
+
+function createSimpleRoute(routes, path) {
+	return (
+		<Route
+			exact
+			key={path}
+			path={path}
+			component={connectToRedux(routes[path])}
+		/> // eslint-disable-line react/jsx-closing-bracket-location
+	);
+}
+
+function composeComplexComponent(route, path) {
+	const Template = route.template;
+	const typeOfTemplate = typeof Template;
+	if (Template && typeOfTemplate !== "function") {
+		throw new Error(
+			`template used in "${path}" path should be correct React ` +
+				`component, but it is variable/expression of "${typeOfTemplate}" type`,
+		);
+	}
+	if (Template) {
+		return () => <Template {...route.templateParams} />;
+	}
+	return <div>Complex component type is not supported</div>;
+}
+
+function createComplexRoute(routes, path) {
+	const route = routes[path];
+	const component = composeComplexComponent(route, path);
+	return (
+		<Route exact key={path} path={path} component={connectToRedux(component)} /> // eslint-disable-line react/jsx-closing-bracket-location
+	);
+}
+
+function addRoute(path, routes) {
+	const routeType = typeof routes[path];
+	if (routeType === "function") {
+		return createSimpleRoute(routes, path);
+	}
+	if (routeType === "object") {
+		return createComplexRoute(routes, path);
+	}
+	return (
+		<Route
+			exact
+			key={path}
+			path={path}
+			component={() => <div>Not implemented yet</div>}
+		/> // eslint-disable-line react/jsx-closing-bracket-location
+	);
 }
 
 function getEnhancers() {
@@ -56,4 +106,9 @@ export function initApplication(routes) {
 		</Provider>,
 		document.getElementById("app"),
 	);
+}
+
+export function componentWithPropTypes(component, proptypes) {
+	component.propTypes = proptypes; // eslint-disable-line no-param-reassign
+	return component;
 }
